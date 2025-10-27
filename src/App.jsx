850d6f58b4bc60b091031de505d7151a21c7c34b
Vedang Dhuri <netvedang@gmail.com>
2025-10-27 13:21:26 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
-import { Toaster } from "@/components/ui/toaster";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Home } from "./pages/Home";
+// import { NotFound } from "./pages/NotFound";
+// import { Toaster } from "@/components/ui/toaster";
+
+import { Navbar } from "./components/Navbar";
+import { ThemeToggle } from "./components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "./components/HeroSection";
+import { AboutSection } from "./components/AboutSection";
+import { SkillsSection } from "./components/SkillsSection";
+import { ProjectsSection } from "./components/ProjectsSection";
+import { ContactSection } from "./components/ContactSection";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
+    // <>
+    //   <Toaster />
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route index element={<Home />} />
+    //       <Route path="*" element={<NotFound />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // </>
+
     <>
-      <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme Toggle */}
+            <ThemeToggle />
+            {/* Background Effects */}
+            <StarBackground />
+      
+            {/* Navbar */}
+            <Navbar />
+            {/* Main Content */}
+            <main>
+              <HeroSection />
+              <AboutSection />
+              <SkillsSection />
+              <ProjectsSection />
+              <ContactSection />
+            </main>
+      
+            {/* Footer */}
+            <Footer />
+          </div>
     </>
   );
 }
