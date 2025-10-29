b8fb6be655bf9a3480d480824e736755cd602b59
Vedang Dhuri <netvedang@gmail.com>
2025-10-29 22:58:03 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio-main",
+  base: "/portfolio",
   // base: process.env.VITE_BASE_PATH || "/portfolio-main",
   resolve: {
     alias: {
